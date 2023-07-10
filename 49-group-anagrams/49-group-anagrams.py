2cabba5293751d9118cd1a2a8c354e178f71ef3e
Ahmed Mohammed <64283607+d3mah-24@users.noreply.github.com>
2023-07-10 21:46:38 +0300

    Time: 109 ms (85.39%), Space: 20 MB (67.45%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result={}
+        for x in strs:
+            temp="".join(sorted(x))
+            if  temp in result:
+                result[temp].append(x)
+            else:
+                result[temp] =[x]
+        return result.values()
+        
