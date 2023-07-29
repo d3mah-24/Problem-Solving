c4e38f5e03d94231c8801b47400b549eda47b6c4
Ahmed Mohammed <64283607+d3mah-24@users.noreply.github.com>
2023-07-29 21:20:36 +0300

    Time: 1949 ms (17.72%), Space: 16.4 MB (27.66%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -5,8 +5,7 @@ class Solution:
 
         while a*a<i:
             i//=2
-            a+=1 
-        if i==1:return 1 
+            a+=1  
         for t in range(0,i*i+1):
             if t*t==x:
                 return t   
