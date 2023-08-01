94cfc9a7de0eb77f64456f94f8d066a67b76091f
Ahmed Mohammed <64283607+d3mah-24@users.noreply.github.com>
2023-08-01 23:00:16 +0300

    Time: 78 ms (97.06%), Space: 17.1 MB (33.66%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -4,17 +4,6 @@ class Solution:
         b=[d.count(min(d)) for d in queries]
         res=[]
         l=len(a)
-        for c in b:
-            left,mid,right=0,len(a)//2,len(a)-1
-            temp=l-bisect.bisect(a,c)
-            # while left<=right:
-            #     mid=left+(right-left)//2
-            #     if a[mid]==c:
-            #         temp=len(a)-mid
-            #     elif c>a[mid]:
-            #         right=mid-1 
-            #     else:
-            #         left=mid+1 
-            res.append(temp)
-            
+        for c in b:  
+            res.append(l-bisect.bisect(a,c))
         return res
