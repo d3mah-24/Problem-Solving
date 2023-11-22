82e217c5b8412a88fb3cc40911f4cf12c8fa1327
Ahmed Mohammed <64283607+d3mah-24@users.noreply.github.com>
2023-11-22 08:45:07 +0300

    Time: 48 ms (74.20%), Space: 16.4 MB (17.03%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -2,9 +2,11 @@ class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         Result=[]
         for x in range(left,right+1):
+            if "0" in str(x):
+                continue
             Result.append(x)
             for a in str(x):
-                if  a=="0" or x%int(a)!=0 :
+                if   x%int(a)!=0 :
                     Result.pop()
                     break  
         return Result
