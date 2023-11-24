5f9ba83925c8c09ee5fed5ca75e409db85631967
Ahmed Mohammed <64283607+d3mah-24@users.noreply.github.com>
2023-11-24 20:31:26 +0300

    Time: 64 ms (41.56%), Space: 16.4 MB (42.49%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,6 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        a=[0]*(n*2)
-        a[::2]=nums[:n]
-        a[1::2]=nums[n:]
-        return  a
+        # a=[0]*(n*2)
+        # a[::2]=nums[:n]
+        # a[1::2]=nums[n:]
+        # return  a
+        Result=[]
+        for a,b in zip(nums[:n],nums[n:]):
+            Result.append(a)
+            Result.append(b)
+        return Result
